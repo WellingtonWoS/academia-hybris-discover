61b7bf6a9f78db3e0827ae55eef0169f2b6395b6
João Gomes <iortim@hotmail.com>
2024-11-04 18:13:37 -0300

    feat: vamo vamo inter :triangular_flag_on_post:

---
+++ TestJoao.java
@@ -0,0 +1,5 @@
+# Boas vindas!
+
+Seja bem vindo a Academia SAP Commerce 2024 da Discover!
+
+Este repositório conterá o material utilizado durante as aulas, os arquivos das tarefas que você realizará, além de alguns exercicios complementares e alguns conteúdos extras, assim você pode revisar se perdeu alguma coisa.
