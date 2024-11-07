d2850e4b14479b188634fb3b6e78a579eef7fdf6
Erik Gaborim <erikgfp@gmail.com>
2024-11-06 23:06:30 -0300

    Update queriesErik.sql

--- queriesErik.sql
+++ queriesErik.sql
@@ -24,15 +24,13 @@ INSERT INTO lojavirtual_hybris.Produto (idProduto, Fornecedor_idFornecedor, Esto
 INSERT INTO lojavirtual_hybris.Produto (idProduto, Fornecedor_idFornecedor, Estoque_idEstoque, TipoProduto_idTipoProduto, nome) VALUES ('128', '6', '6', '7', 'Produto 6');
 
 #2
--- SELECT * FROM lojavirtual_hybris.Produto ORDER BY idProduto LIMIT 3;
+SELECT * FROM lojavirtual_hybris.Produto ORDER BY idProduto LIMIT 3;
 
 #3
--- SELECT produto.idProduto, produto.nome, estoque.quantidade FROM lojavirtual_hybris.Produto AS produto JOIN lojavirtual_hybris.Estoque AS estoque ON produto.Estoque_idEstoque = estoque.idEstoque ORDER BY estoque.quantidade DESC;
+SELECT produto.idProduto, produto.nome, estoque.quantidade FROM lojavirtual_hybris.Produto AS produto JOIN lojavirtual_hybris.Estoque AS estoque ON produto.Estoque_idEstoque = estoque.idEstoque ORDER BY estoque.quantidade DESC;
 
 #4
--- SELECT * FROM lojavirtual_hybris.Produto WHERE Fornecedor_idFornecedor = 2;
-
-SELECT * FROM lojavirtual_hybris.Produto
+SELECT * FROM lojavirtual_hybris.Produto WHERE Fornecedor_idFornecedor = 2;
 
 #5
--- DELETE FROM lojavirtual_hybris.Produto AS produto WHERE produto.Fornecedor_idFornecedor = 3 AND produto.nome = 'Produto E';
+DELETE FROM lojavirtual_hybris.Produto AS produto WHERE produto.Fornecedor_idFornecedor = 3 AND produto.nome = 'Produto E';
